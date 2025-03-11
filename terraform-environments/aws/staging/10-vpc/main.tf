34c007cb02f695ebb20c81f2652b8fc57e2cd1ba
dicky <dicky@example.com>
2025-03-11 16:27:47 +0700

    format main.tf

--- terraform-environments/aws/staging/10-vpc/main.tf
+++ terraform-environments/aws/staging/10-vpc/main.tf
@@ -39,9 +39,9 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.19.0"
 
-  azs              = ["ap-southeast-3a", "ap-southeast-3b", "ap-southeast-3c"]
-  cidr             = "10.0.0.0/16"
-  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  tags             = local.tags
+  azs             = ["ap-southeast-3a", "ap-southeast-3b", "ap-southeast-3c"]
+  cidr            = "10.0.0.0/16"
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  tags            = local.tags
 }
