eb8486259b3ca85e4e0517b0fc826046bd835efe
dicky <dicky@example.com>
2025-03-11 14:44:34 +0700

    Update provider version and module source

--- terraform-environments/aws/staging/vpc/main.tf
+++ terraform-environments/aws/staging/vpc/main.tf
@@ -5,7 +5,7 @@ locals {
     ops_env              = "${local.environment_name}"
     ops_managed_by       = "terraform",
     ops_source_repo      = "kubernetes-ops",
-    ops_source_repo_path = "terraform-environments/aws/${local.environment_name}/10-vpc",
+    ops_source_repo_path = "aws/${local.environment_name}/vpc",
     ops_owners           = "devops",
   }
 }
@@ -14,7 +14,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.37.0"
+      version = ">= 5.90.1"
     }
   }
 
@@ -36,14 +36,12 @@ provider "aws" {
 # VPC
 #
 module "vpc" {
-  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/vpc?ref=v1.0.30"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
 
-  aws_region       = local.aws_region
-  azs              = ["us-east-1a", "us-east-1c", "us-east-1d"]
-  vpc_cidr         = "10.0.0.0/16"
+  azs              = ["ap-southeast-3a", "ap-southeast-3b", "ap-southeast-3c"]
+  cidr             = "10.0.0.0/16"
   private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  environment_name = local.environment_name
-  cluster_name     = local.environment_name
   tags             = local.tags
 }
