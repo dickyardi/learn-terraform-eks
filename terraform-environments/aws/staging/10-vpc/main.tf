eee00ea97caa74baf634a4690087b201e33bc35d
dicky <dicky@example.com>
2025-03-11 16:32:54 +0700

    Update provider version

--- terraform-environments/aws/staging/10-vpc/main.tf
+++ terraform-environments/aws/staging/10-vpc/main.tf
@@ -13,8 +13,8 @@ locals {
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.90.1"
+      source = "hashicorp/aws"
+      version = "5.90.1"
     }
   }
 
