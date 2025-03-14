91317c098bb958c058680e4fe0c39db27844c581
dicky <dicky@example.com>
2025-03-14 10:20:31 +0700

    update provider version

--- terraform-environments/aws/staging/25-eks-cluster-autoscaler/main.tf
+++ terraform-environments/aws/staging/25-eks-cluster-autoscaler/main.tf
@@ -21,7 +21,11 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.3.0"
+      version = ">= 2.7.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.1"
     }
   }
 
