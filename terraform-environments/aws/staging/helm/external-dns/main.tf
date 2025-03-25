d1fbde04fd3a2ab9e04fbe0cbc3eee841c696128
dicky <dicky@example.com>
2025-03-25 17:07:02 +0700

    Fix external dns

--- terraform-environments/aws/staging/helm/external-dns/main.tf
+++ terraform-environments/aws/staging/helm/external-dns/main.tf
@@ -5,11 +5,9 @@ locals {
     ops_env              = "${local.environment_name}"
     ops_managed_by       = "terraform",
     ops_source_repo      = "kubernetes-ops",
-    ops_source_repo_path = "terraform-environments/aws/${local.environment_name}/20-eks",
+    ops_source_repo_path = "terraform-environments/aws/${local.environment_name}/helm/external-dns",
     ops_owners           = "devops",
   }
-  helm_repository = "https://kubernetes-sigs.github.io/external-dns/"
-  official_chart_name = "external-dns"
 }
 
 terraform {
@@ -20,10 +18,6 @@ terraform {
     }
     random = {
       source = "hashicorp/random"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.19.0"
     }
     helm = {
       source  = "hashicorp/helm"
@@ -67,17 +61,6 @@ data "terraform_remote_state" "route53_hosted_zone" {
   }
 }
 
-data "aws_eks_cluster_auth" "main" {
-  name = local.environment_name
-}
-
-provider "kubectl" {
-  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.main.token
-  load_config_file       = false
-}
-
 #
 # EKS authentication
 # # https://registry.terraform.io/providers/hashicorp/helm/latest/docs#exec-plugins
@@ -93,76 +76,20 @@ provider "helm" {
   }
 }
 
-
-module "iam_assumable_role_admin" {
-  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "3.6.0"
-  create_role                   = true
-  role_name                     = "${local.official_chart_name}-${var.cluster_name}"
-  provider_url                  = replace(var.eks_cluster_oidc_issuer_url, "https://", "")
-  role_policy_arns              = [aws_iam_policy.iam_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.k8s_namespace}:${local.official_chart_name}"]
-}
-
-resource "aws_iam_policy" "iam_policy" {
-  name_prefix = "${local.official_chart_name}-${var.cluster_name}"
-  description = "EKS ${local.official_chart_name} policy for cluster ${var.eks_cluster_id}"
-  policy      = data.aws_iam_policy_document.iam_policy_document.json
-}
-
-# IAM Role policy doc: https://github.com/kubernetes-sigs/external-dns/blob/master/docs/tutorials/aws.md
-data "aws_iam_policy_document" "iam_policy_document" {
-  statement {
-    sid    = "k8sExternalDNS"
-    effect = "Allow"
-
-    actions = [
-      "route53:ChangeResourceRecordSets",
-    ]
-
-    resources = ["arn:aws:route53:::hostedzone/${var.route53_hosted_zones}"]
-  }
-
-  statement {
-    sid    = "k8sExternalDNS2"
-    effect = "Allow"
-
-    actions = [
-        "route53:ListHostedZones",
-        "route53:ListResourceRecordSets",
-    ]
-
-    resources = ["*"]
-  }
-}
-
-data "aws_caller_identity" "current" {}
-
 #
-# Helm values
+# Helm - external-dns
 #
-data "template_file" "helm_values" {
-  template = file("${path.module}/helm_values.tpl.yaml")
-  vars = {
-    awsAccountID       = data.aws_caller_identity.current.account_id
-    clusterName        = var.cluster_name
-    serviceAccountName = local.official_chart_name
-    chartName          = local.official_chart_name
-  }
-}
-
 module "external-dns" {
-  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/helm/helm_generic?ref=v1.0.27"
+  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/helm/external-dns?ref=v1.0.28"
 
-  repository          = local.helm_repository
-  official_chart_name = local.official_chart_name
-  user_chart_name     = var.user_chart_name
-  helm_version        = var.helm_chart_version
-  namespace           = var.k8s_namespace
-  helm_values         = data.template_file.helm_values.rendered
-  helm_values_2       = var.helm_values_2
+  aws_region                  = local.aws_region
+  cluster_name                = local.environment_name
+  eks_cluster_id              = data.terraform_remote_state.eks.outputs.cluster_id
+  eks_cluster_oidc_issuer_url = data.terraform_remote_state.eks.outputs.cluster_oidc_issuer_url
+  route53_hosted_zones        = data.terraform_remote_state.route53_hosted_zone.outputs.zone_id
+  helm_values_2               = file("${path.module}/values.yaml")
 
   depends_on = [
-    module.iam_assumable_role_admin
+    data.terraform_remote_state.eks
   ]
 }
