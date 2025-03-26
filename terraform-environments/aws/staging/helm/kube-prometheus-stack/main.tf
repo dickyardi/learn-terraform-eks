04bae008973ad1cb3ffef8bc884291a057bd2275
dicky <dicky@example.com>
2025-03-26 15:44:03 +0700

    Add kubectl provider

--- terraform-environments/aws/staging/helm/kube-prometheus-stack/main.tf
+++ terraform-environments/aws/staging/helm/kube-prometheus-stack/main.tf
@@ -19,6 +19,10 @@ terraform {
     random = {
       source = "hashicorp/random"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.19.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.7.1"
